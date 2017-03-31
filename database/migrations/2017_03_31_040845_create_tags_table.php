19e8ab17c392affe1c96755700c34faca9adc5c5
Jorge <jorge.alfmur@gmail.com>
2017-03-30 23:56:31 -0500

    creamos la tabla pivot porque tenemos una relacion muchos a muchos pero la creamos dentro de la migracion tags

--- database/migrations/2017_03_31_040845_create_tags_table.php
+++ database/migrations/2017_03_31_040845_create_tags_table.php
@@ -15,7 +15,20 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             /* columnas de la tabla*/
-            $tabla->string('name');
+            $table->string('name');
+
+            $table->timestamps();
+        });
+
+        /*crear tabla pivot*/
+        Schema::create('article_tag', function (Blueprint $table){
+            $table->increments('id');
+            /*Foreign key categories*/
+            $table->integer('article_id')->unsigned();
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+            /*Foreign key tags*/
+            $table->integer('tag_id')->unsigned();
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             
             $table->timestamps();
         });
