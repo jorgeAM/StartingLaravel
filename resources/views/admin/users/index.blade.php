748204e8bb3db10e50b888851b349a4e1cee24de
Jorge <jorge.alfmur@gmail.com>
2017-04-03 22:43:15 -0500

    mejoramos la vista del index

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,9 +1,32 @@
 @extends('admin.template.main')
 @section('title', 'Lista de Usuarios')
 @section('content')
-	@foreach ($users as $user)
-		<p>This is user {{ $user->name }}</p>
-	@endforeach
+	
+	<a href="{{route('admin.user.create')}}" class="btn btn-info">Registrar un nuevo usuario</a><br><br>
+	<table class="table table-hover">
+  		<thead>
+  			<th>ID</th>
+  			<th>Nombre</th>
+  			<th>Correo Electronico</th>
+  			<th>Tipo</th>
+  			<th>Acción</th>
+  		</thead>
+
+  		<tbody>
+  			@foreach ($users as $user)
+  				<tr>
+  					<td>{{ $user->id }}</td>
+  					<td>{{ $user->name }}</td>
+  					<td>{{ $user->email }}</td>
+  					<td>{{ $user->type }}</td>
+  					<td>
+  						<a href="" class="btn btn-danger"></a>
+  						<a href="" class="btn btn-warning"></a>
+  					</td>
+  				</tr>
+			@endforeach
+  		</tbody>
+	</table>
 
 	{!! $users->render() !!}
 @endsection
